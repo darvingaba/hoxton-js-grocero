b4585c12f1ba683b480866c9e8220f17f61126a5
darvingaba <darvingaba1@gmail.com>
2022-06-27 23:59:31 +0200

    updates

--- index.js
+++ index.js
@@ -5,70 +5,70 @@ let state = {
       id: 1,
       name: "beetroot",
       price: 0.35,
-      itemNumber: 3,
+      itemNumber: 0,
       image: "assets/icons/001-beetroot.svg",
     },
     {
       id: 2,
       name: "carrot",
       price: 0.35,
-      itemNumber: 5,
+      itemNumber: 0,
       image: "assets/icons/002-carrot.svg",
     },
     {
       id: 3,
       name: "apple",
       price: 0.35,
-      itemNumber: 3,
+      itemNumber: 0,
       image: "assets/icons/003-apple.svg",
     },
     {
       id: 4,
       name: "apricot",
       price: 0.35,
-      itemNumber: 5,
+      itemNumber: 0,
       image: "assets/icons/004-apricot.svg",
     },
     {
       id: 5,
       name: "avocado",
       price: 0.35,
-      itemNumber: 3,
+      itemNumber: 0,
       image: "assets/icons/005-avocado.svg",
     },
     {
       id: 6,
       name: "banana",
       price: 0.35,
-      itemNumber: 5,
+      itemNumber: 0,
       image: "assets/icons/006-bananas.svg",
     },
     {
       id: 7,
       name: "bell-pepper",
       price: 0.35,
-      itemNumber: 7,
+      itemNumber: 0,
       image: "assets/icons/007-bell-pepper.svg",
     },
     {
       id: 8,
       name: "berry",
       price: 0.35,
-      itemNumber: 3,
+      itemNumber: 0,
       image: "assets/icons/008-berry.svg"
     },
     {
       id: 9,
       name: "blueberry",
       price: 0.35,
-      itemNumber: 10,
+      itemNumber: 0,
       image: "assets/icons/009-blueberry.svg"
     },
     {
       id: 10,
       name: "eggplant",
       price: 0.35,
-      itemNumber: 4,
+      itemNumber: 0,
       image: "assets/icons/010-eggplant.svg"
     },
   ],
@@ -81,27 +81,26 @@ let state = {
     divEl.className = "store--item-icon";
     let imgEl = document.createElement("img");
 
-    // state.items.forEach(function(item) {
-    //   imgEl.src = item.image;
-    //   imgEl.alt = item.name;
-    // });
     
-    imgEl.src = item.image;
-    imgEl.alt = item.name;
-    // }
+      imgEl.src = item.image;
+      imgEl.alt = item.name;
 
     divEl.appendChild(imgEl);
     let buttonEl = document.createElement("button");
     buttonEl.textContent = "Add to cart";
     
+    
     buttonEl.addEventListener("click", function() {
-      cartItem(item);
+        item.itemNumber++;
+        cartItem(item);
+
     })
 
     liEl.append(divEl, buttonEl);
 
     let storeUl = document.querySelector(".item-list.store--item-list");
     storeUl.append(liEl);
+    this.removeEventListener("click", createItem);
   }
 
 for(let item of state.items) {
@@ -115,6 +114,7 @@ function cartItem(item){
   let imgEl = document.createElement("img");
   imgEl.className = "cart--item-icon";
   imgEl.src = item.image;
+  imgEl.alt = item.name;
 
   let pEl = document.createElement("span");
   pEl.textContent = item.name.toUpperCase();
@@ -127,11 +127,7 @@ function cartItem(item){
   })
   let numberOfItems = document.createElement("span");
   numberOfItems.textContent = item.itemNumber;
-  
-  if(item.itemNumber < 1) {
-    deleteItem();
-    render();
-  }
+  console.log(numberOfItems);
 
   let buttonEl2 = document.createElement("button");
   buttonEl2.textContent = "+";
@@ -145,23 +141,34 @@ function cartItem(item){
   liEl.append(imgEl,pEl, buttonEl, numberOfItems, buttonEl2);
   cartUl.append(liEl);
 }
+function getPrice(item) {
+  return item.price;
+}
+function getNumberOfItems(item) {
+  return item.itemNumber;
+}
 function totalPrice() {
-  let price = document.querySelector(".total-number");
-
+  let spanEl = document.querySelector(".total-number");
+  let total = 0;
+  spanEl.innerHTML = "";
+  for (let item of state.items) {
+    total += getNumberOfItems(item) * getPrice(item);
+  }
+  spanEl.textContent = `£${total.toFixed(2)}`;
+  console.log(total);
 }
 
 console.log(state.items);
 
-function deleteItem(item) { 
-    item.remove(item);
-    }
+
+
 
 
 function render() { 
 
   createItem();
   cartItem();
-
+  totalPrice();
 }
 
 render();
